6691fc3fd3067e026870b34ad8c7fd97ea502450
sohamLolage <lolagesoham@gmail.com>
2021-06-20 18:49:42 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { TodoComponent } from './mycomponent/todo/todo.component';
 import { TodoItemComponent } from './mycomponent/todo-item/todo-item.component';
 import { AddTodoComponent } from './mycomponent/add-todo/add-todo.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './registrationportal/home/home.component';
+import { LoginComponent } from './registrationportal/login/login.component';
+import { RegistrationComponent } from './registrationportal/registration/registration.component';
+import { NavbarComponent } from './registrationportal/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoItemComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
